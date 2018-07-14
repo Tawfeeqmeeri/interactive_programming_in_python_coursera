b2e7a2dbdd598b87e0a7beefa4c0083985f5688e
Tawfeeq <Tawfeeq@tawfeeqs-air.speedport_w_724v_typ_a_05011603_05_028>
2018-07-15 01:16:57 +0200

    mystical octosphere

--- course/week1/mystical_octosphere.py
+++ course/week1/mystical_octosphere.py
@@ -1,12 +1,43 @@
 import random 
 import time 
 
+# This game is based on a common toy. It is a 
+# round black ball with a clear plastic window. 
+# The ball is filled with murky blue liquid
+# and you use it as a fortune teller. You ask 
+# a yes-or-no question and shake the ball. There 
+# is a white many-sided die inside with answers, 
+# and when you stop shaking, one of the sides
+# floats up and is readable against the window.
+
+# Here is a sample of the kind of
+# output this program should produce:
+
+# Your question was... Will I get rich?
+# You shake the mystical octosphere.
+# The cloudy liquid swirls, and a reply comes into view...
+# The mystical octosphere says... Probably yes.
+
+# The possible numbers are between 0 through 7 inclusive
+# (that means 0, 1, 2, 3, 4, 5, 6, or 7)
+# and each number should translate to a fortune
+# that would be the answer to a yes or no question.
+#
+# My suggested fortunes are:
+# 0 - Yes, for sure!
+# 1 - Probably yes.
+# 2 - Seems like yes...
+# 3 - Definitely not!
+# 4 - Probably not.
+# 5 - I really doubt it...
+# 6 - Not sure, check back later!
+# 7 - I really can't tell
+#
+# If somehow the function gets a number other than those 8
+# it should send back a string saying that
+# something was wrong with the input.
 
 def number_to_fortune(number):
-    # Fill in your code here.
-    # Use an if...elif...else statement
-    # to check each of the numbers between 0 and 7
-    # and return the fortune as a string.
      if number == 0:
         return "Yes, for sure!"
      elif number == 1:
@@ -26,18 +57,6 @@ def number_to_fortune(number):
      else:
         return "Incorrect number! Choose between 0-7"
     
-# TEST SECTION...    
-# Uncomment this code to test if your number_to_fortune
-# helper function is working. Highlight it all
-# and hit control-shift-k to uncomment it all at once.
-""" number_to_fortune(0)
-number_to_fortune(1)
-number_to_fortune(2)
-number_to_fortune(4)
-number_to_fortune(5)
-number_to_fortune(6)
-number_to_fortune(7)
-number_to_fortune(19) """
 
 def mystical_octosphere(question):
    print ("Your question was: "+ question)
